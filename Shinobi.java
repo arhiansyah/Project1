8ebd3d0ec60ab4c944a7efacda65d802af80a5db
arhiansyah <55195854+arhiansyah@users.noreply.github.com>
2020-03-22 20:01:21 +0700

    Create Shinobi.java

---
+++ Shinobi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan1;
+
+/**
+ *
+ * @author RIVANDWIARHIANSYAH-18102068
+ * Shinobi.java 
+ */
+public class Shinobi {
+    private String Noregistrasi;
+    private String Nama;
+    private String Peringkat;
+
+    public void setNoregistrasi(String Noregistrasi) {
+        this.Noregistrasi = Noregistrasi;
+    }
+
+    public String getNoregistrasi() {
+        return Noregistrasi;
+    }
+
+     public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+    
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setPeringkat(String Peringkat) {
+        this.Peringkat = Peringkat;
+    }
+    
+    public String getPeringkat() {
+        return Peringkat;
+    }
+}
